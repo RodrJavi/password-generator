f12903572befa5ce4fca569c4007ecd60bce7793
Javi <nb2javi@gmail.com>
2023-07-04 18:22:59 -0500

    Changed variable names

--- assets/script.js
+++ assets/script.js
@@ -2,23 +2,27 @@
 
 function generatePassword() {
   var passwordOutput;
-  var lowerCase;
-  var upperCase;
-  var numbers;
-  var specialCharacters;
+  var lowerCaseBoolean;
+  var upperCaseBoolean;
+  var numbersBoolean;
+  var specialCharactersBoolean;
+  var lowerCaseCharacters = ["a", "b"];
   function getLength() {
     let length = window.prompt("Pick a length from 8 to 128 characters");
     return length;
   }
   var passwordLength = getLength();
+
   if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
     window.alert("Please enter a value from 8 to 128");
     passwordOutput = "Try generating again";
   } else {
-    lowerCase = window.confirm("Does this have lowercase letters?");
-    upperCase = window.confirm("Does this have uppercase letters?");
-    numbers = window.confirm("Does this have numbers?");
-    specialCharacters = window.confirm("Does this have special characters?");
+    lowerCaseBoolean = window.confirm("Does this have lowercase letters?");
+    upperCaseBoolean = window.confirm("Does this have uppercase letters?");
+    numbersBoolean = window.confirm("Does this have numbers?");
+    specialCharactersBoolean = window.confirm(
+      "Does this have special characters?"
+    );
   }
   console.log(lowerCase, upperCase, numbers, specialCharacters);
   return passwordOutput;
